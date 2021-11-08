3fcdbfc55840462bc2287251e7b8e0e65df9d1ff
ansarali41 <ansar.ict14@gmail.com>
2021-11-08 21:00:24 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Components/Home/Home/Home';
+import Order from './Components/Dashboard/Order/Order';
+import ServiceList from './Components/Dashboard/ServiceList/ServiceList';
+import PageNotFound from './Components/PageNotFound/PageNotFound';
+import Progress from './Components/Progress/Progress';
+import Login from './Components/Login/Login';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import ServiceListAdmin from './Components/Admin/ServiceListAdmin/ServiceListAdmin';
+import AddService from './Components/Admin/AddService/AddService';
+import Dashboard from './Components/Dashboard/Dashboard/Dashboard';
+import About from './Components/About/About';
+
+export const UserContext = createContext();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [user, setUser] = useState({
+    //     displayName: '',
+    //     email: '',
+    // });
+    const userDetails = localStorage.getItem('user');
+    let parseUser = JSON.parse(userDetails);
+    if (!parseUser) {
+        parseUser = {
+            displayName: '',
+            email: '',
+            photo: '',
+        };
+    }
+
+    const [user, setUser] = useState(parseUser);
+    // const [user, setUser] = useState({
+    //     displayName: 'Ansar A',
+    //     email: 'ansar.sdp75@gmail.com',
+    //     photo: 'https://lh3.googleusercontent.com/a-/AOh14GjGDWDvTge5bjoANXcIOXqWIEnXevfvamt7FRm82w=s96-c',
+    // });
+
+    return (
+        <UserContext.Provider className="container" value={[user, setUser]}>
+            <Router>
+                <Switch>
+                    <PrivateRoute path="/dashboard/:id">
+                        <Dashboard />
+                    </PrivateRoute>
+
+                    <PrivateRoute path="/order">
+                        <Order></Order>
+                    </PrivateRoute>
+
+                    <PrivateRoute path="/serviceList">
+                        <ServiceList></ServiceList>
+                    </PrivateRoute>
+
+                    <PrivateRoute path="/serviceListAdmin">
+                        <ServiceListAdmin />
+                    </PrivateRoute>
+
+                    <PrivateRoute path="/addService">
+                        <AddService />
+                    </PrivateRoute>
+
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+
+                    <Route path="/home">
+                        <Home></Home>
+                    </Route>
+
+                    <Route path="/progress">
+                        <Progress></Progress>
+                    </Route>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+
+                    <Route exact path="/">
+                        <Home></Home>
+                    </Route>
+
+                    <Route path="*">
+                        <PageNotFound></PageNotFound>
+                    </Route>
+                </Switch>
+            </Router>
+        </UserContext.Provider>
+    );
 }
 
 export default App;
---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyBAoYmjLkhF8PbSsigXoMGHzdqY7icO5MY',
+    authDomain: 'e-hotel-b6aa5.firebaseapp.com',
+    projectId: 'e-hotel-b6aa5',
+    storageBucket: 'e-hotel-b6aa5.appspot.com',
+    messagingSenderId: '951877376892',
+    appId: '1:951877376892:web:c03871ee4ac8a9379422f9',
+};
+
+export default firebaseConfig;
